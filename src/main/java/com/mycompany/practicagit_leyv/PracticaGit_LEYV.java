159a1d1130f4ddbccde9c33e85197f50d9ca0a55
dzibalejandro <alejandro.dzib_pm24311@cecyte.edu.mx>
2026-03-03 07:40:02 -0600

    feat: estructura inicial del proyecto

---
+++ src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_leyv;
+
+/**
+ *
+ * @author Pomuch 5
+ */
+public class PracticaGit_LEYV {
+
+    public static void main(String[] args) {
+        System.out.println("--- Sistema de Gestión");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
