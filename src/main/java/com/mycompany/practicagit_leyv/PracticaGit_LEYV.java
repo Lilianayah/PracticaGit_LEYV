f0a3e016c0e8860a5a2e8c79a9d7dd8988586ba6
dzibalejandro <alejandro.dzib_pm24311@cecyte.edu.mx>
2026-03-09 07:59:57 -0600

    fix: añadida calculadora básica.

--- src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
+++ src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
@@ -13,6 +13,25 @@ public class PracticaGit_LEYV {
     public static void main(String[] args) {
         System.out.println("--- Sistema de Gestión");
         System.out.println("Bienvenido, Usuario Estudiante.");
+        
+        int n1, n2 , Suma;
+        
+        System.out.print("Introduce el primer número");
+        n1 = cin.nextInt ();
+        
+        System.out.print("Introduce segundo número");
+        n2 = cin.nextInt ();
+        
+        Suma = n1 + n2;
+        
+        System.out.println("El resultado de la suma es: " + Suma);
+        
+        
+        
+        
+        
+        
+        
    
         
         
