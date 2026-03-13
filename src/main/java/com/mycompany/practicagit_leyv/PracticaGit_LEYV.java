97ef5b20af10c74398475269ec6c870f0bac06a2
dzibalejandro <alejandro.dzib_pm24311@cecyte.edu.mx>
2026-03-13 10:34:41 -0600

    feat: estructura inicial del proyecto.

--- src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
+++ src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
@@ -11,35 +11,8 @@ package com.mycompany.practicagit_leyv;
 public class PracticaGit_LEYV {
 
     public static void main(String[] args) {
-        System.out.println("--- Sistema de Gestión");
+        System.out.println("--- Sistema de Gestión de alumnos ---");
         System.out.println("Bienvenido, Usuario Estudiante.");
-        
-        int n1, n2 , Suma;
-        
-        System.out.print("Introduce el primer número");
-        n1 = cin.nextInt ();
-        
-        System.out.print("Introduce segundo número");
-        n2 = cin.nextInt ();
-        
-        Suma = n1 + n2;
-        
-        System.out.println("El resultado de la suma es: " + Suma);
-        
-        
-        
-        
-        
-        
-        
-   
-        
-        
-        
-        
-        
-        
-        
         
         
     }
