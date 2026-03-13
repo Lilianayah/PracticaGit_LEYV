1c1a05eb38f0fae032653567069058e17e748035
dzibalejandro <alejandro.dzib_pm24311@cecyte.edu.mx>
2026-03-13 10:46:46 -0600

    fix: añadida calculadora básica.

--- src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
+++ src/main/java/com/mycompany/practicagit_leyv/PracticaGit_LEYV.java
@@ -1,29 +1,34 @@
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-package com.mycompany.practicagit_leyv;
 
 /**
  *
  * @author Pomuch 5
  */
-public class PracticaGit_LEYV {
+public class SumaNum {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("--- Sistema de Gestión de alumnos ---");
-        System.out.println("Bienvenido, Usuario Estudiante.");
-         int n1, n2 ,Suma ;
+   Scanner cin = new Scanner (System.in);
+      
+   
+        int n1, n2, suma;
         
-        System.out.print("Introduce el primer número");
-        n1 = cin.nextInt ();
         
-        System.out.print("Introduce segundo número");
-        n2 = cin.nextInt ();
+    System.out.print("Introduce primer número: ");
+    n1 = cin.nextInt ();    
+    
+    System.out.print("Introduce segundo número: ");
+     n2 = cin.nextInt();
+     
+     suma = n1 + n2;
         
-        Suma = n1 + n2;
+     
+     System.out.println("El resultado es: "+ suma);
         
-        System.out.println("El resultado de la suma es: " + Suma);
-        
-    }
-}
